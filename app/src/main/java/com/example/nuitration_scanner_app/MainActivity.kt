687ff14e83c09e0497e37baa6a6a4d947b97e72a
Moussa Kabalan <moussakabaln@gmail.com>
2026-04-15 00:28:45 -0400

    Deploy

---
+++ app/src/main/java/com/example/nuitration_scanner_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nuitration_scanner_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
