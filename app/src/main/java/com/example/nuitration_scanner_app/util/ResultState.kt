4aa3e1d630afa8bdeac4bfdac84df20c5aa52f0f
Moussa Kabalan <moussakabaln@gmail.com>
2026-04-15 00:35:06 -0400

    Restart

--- app/src/main/java/com/example/nuitration_scanner_app/util/ResultState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.nuitration_scanner_app.util
-
-sealed class ResultState<out T> {
-    data object Loading : ResultState<Nothing>()
-    data class Success<T>(val data: T) : ResultState<T>()
-    data class Error(val message: String) : ResultState<Nothing>()
-}
